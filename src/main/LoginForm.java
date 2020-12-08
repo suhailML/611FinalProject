6009bc758c90b9e355cba03b395536c0e24800ce
ejbosia <ejbosia@gmail.com>
2020-12-07 22:28:41 -0500

    Ugly account

--- src/main/LoginForm.java
+++ src/main/LoginForm.java
@@ -1,5 +1,4 @@
 import javax.swing.*;
-import javax.swing.border.EmptyBorder;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
--- src/main/UserForm.java
+++ src/main/UserForm.java
@@ -5,20 +5,41 @@ public class UserForm extends JFrame {
 
     public UserForm(String text){
 
-        GridLayout layout = new GridLayout(1,1);
+        GridLayout frameLayout = new GridLayout(1,3);
+        frameLayout.setHgap(10);
+        setLayout(frameLayout);
+
+        GridLayout layout = new GridLayout(3,1);
         layout.setHgap(10);
         layout.setVgap(10);
 
-        JPanel panel = new JPanel(layout);
+        JPanel panelOutput = new JPanel(new GridLayout(1,1));
+        JPanel panelAccounts = new JPanel();
+        JPanel panelActions = new JPanel(layout);
 
 
+        panelOutput.add(new JTextArea("USERNAME: " + text));
 
-        panel.add(new JLabel("USERNAME: " + text));
+        String[] test = {"A", "B", "C", "D", "E"};
+        JList<String> comboTest = new JList<String>(test);
 
-        add(panel, BorderLayout.CENTER);//adding button on frame
+        panelAccounts.add(new JLabel("Account Picker"));
+        panelAccounts.add(comboTest);
 
-        setTitle("Login Form");
-        setSize(400,300);
+        JButton buttonAddAccount = new JButton("Add Account");
+        JButton buttonViewAccount = new JButton("View Account");
+        JButton buttonDeleteAccount = new JButton("Delete Account");;
+
+        panelActions.add(buttonAddAccount);
+        panelActions.add(buttonViewAccount);
+        panelActions.add(buttonDeleteAccount);
+
+        add(panelOutput);//adding button on frame
+        add(panelAccounts);//adding button on frame
+        add(panelActions);//adding button on frame
+
+        setTitle("User View Form");
+        setSize(600,400);
         //setLayout(null);
         setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE);
         setVisible(true);
