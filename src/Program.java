6a325b8d6288806917c11f664ce3ee68e1115015
Tristan Marchand <tmarch@bu.edu>
2020-12-14 13:25:01 -0500

    first finished startup code

--- src/Program.java
+++ src/Program.java
@@ -29,7 +29,7 @@ public class Program
 
         List<List<String>> customerCredentials = db.getAllCustomerCredentials();
         HashMap<String,ArrayList<String>> accountMap = db.getAllUsersAndAccounts();
-        System.out.println(customerCredentials);
+        List<Customer> customers = new ArrayList<Customer>();
         for (List<String> credentials : customerCredentials)
         {
             String ID = credentials.get(4);
@@ -45,7 +45,7 @@ public class Program
                 List<Transaction> transactions = new ArrayList<Transaction>();
                 for (List<String> transactionInfo : allTransactionInfo)
                 {
-                    Transaction transaction;
+                    Transaction transaction = null;
                     switch(transactionInfo.get(0).toUpperCase())
                     {
                         case "WITHDRAW":
@@ -56,52 +56,54 @@ public class Program
                             break;
                         case "TRANSFER":
                             transaction = transactionFactory.getExistingTransfer(Integer.parseInt(transactionInfo.get(3)), Double.parseDouble(transactionInfo.get(2)), accountID, transactionInfo.get(4), transactionInfo.get(5));
+                            break;
                     }
 
                     transactions.add(transaction);
                 }
                 transactionHistory.setTransactions(transactions);
 
-                BankAccount account;
-                if (accountCredential.get(0).equalsIgnoreCase("CHECKING"))
+                BankAccount account = null;
+                if (accountCredential.get(3).equalsIgnoreCase("CHECKING"))
                 {
-                    account = bankAccountFactory.createExistingCheckingAccount(accountCredential.get(1), accountID, accountCredential.get(2), Double.parseDouble(accountCredential.get(3)), transactionHistory);
+                    account = bankAccountFactory.createExistingCheckingAccount(accountCredential.get(0), accountID, accountCredential.get(1), Double.parseDouble(accountCredential.get(2)), transactionHistory);
                 }
-                else if (accountCredential.get(0).equalsIgnoreCase("SAVINGS"))
+                else if (accountCredential.get(3).equalsIgnoreCase("SAVINGS"))
                 {
-                    account = bankAccountFactory.createExistingSavingsAccount(accountCredential.get(1), accountID, accountCredential.get(2), Double.parseDouble(accountCredential.get(3)), transactionHistory);
+                    account = bankAccountFactory.createExistingSavingsAccount(accountCredential.get(0), accountID, accountCredential.get(1), Double.parseDouble(accountCredential.get(2)), transactionHistory);
                 }
 
                 accounts.add(account);
             }
-            // create all accounts
             
-
-
             ArrayList<List<String>> loanCredentials = db.getLoans(ID);
 
             List<Loan> loans = new ArrayList<Loan>();
             for (List<String> loanCredential : loanCredentials)
             {
-                
-
-
-                Loan loan = loanFactory.createExistingLoan(bank, lendee, loanID, initialValue, presentValue, interestRate, collateral);
+                Loan loan = loanFactory.createExistingLoan(bank, getAccountById(accounts, loanCredential.get(1)), loanCredential.get(2), Double.parseDouble(loanCredential.get(3)), Double.parseDouble(loanCredential.get(4)), Double.parseDouble(loanCredential.get(5)), loanCredential.get(6));
+                loans.add(loan);
             }
 
-
-
-
-            //Customer customer = userFactory.createExistingCustomer(credentials.get(0), credentials.get(1), credentials.get(4), credentials.get(2), credentials.get(3));
-            //employees.add(employee);
+            Customer customer = userFactory.createExistingCustomer(credentials.get(0), credentials.get(1), credentials.get(4), credentials.get(2), credentials.get(3), accounts, loans);
+            customers.add(customer);
         }
         
+        // initialize settings and reserves
 
-        return bank;
+        return bank; 
     }
 
-    public static void main(String[] args)
+    private static BankAccount getAccountById(List<BankAccount> accounts, String accountID)
     {
-        startup();
+        for (BankAccount account : accounts)
+        {
+            if (accountID.equals(account.getAccountID()))
+            {
+                return account;
+            }
+        }
+
+        return null;
     }
 }
