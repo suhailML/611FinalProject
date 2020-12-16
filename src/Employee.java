12f7274fe942bdf8748fe2e473eb5e0bbe20ac61
Tristan Marchand <tmarch@bu.edu>
2020-12-15 19:38:40 -0500

    comments

--- src/Employee.java
+++ src/Employee.java
@@ -1,3 +1,12 @@
+/*
+File: Employee.java
+Developer: Tristan Marchand, Evan Boria
+Email: tmarch@bu.edu
+Last Edited: Wednesday, December 16, 2020
+
+Description: Subclass of User representing an Employee. Has access to all data in bank
+*/
+
 public class Employee extends User
 {
     /*
